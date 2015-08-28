cde0226d081b599da270ac51fad95f1205d13693
DasH <273056333@qq.com>
2015-08-28 15:18:10 +0800

    commit
    commit

--- config/db_bk.php
+++ config/db_bk.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=520jgs',
+    'dsn' => 'mysql:host=localhost;dbname=jgsoo',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
