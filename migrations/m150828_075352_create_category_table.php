3359a646da4051accc5f7a40489a94bd2ef5d739
DasH <273056333@qq.com>
2015-08-28 16:49:04 +0800

    migration
    创建migration

---
+++ migrations/m150828_075352_create_category_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150828_075352_create_category_table extends Migration
+{
+    public function up()
+    {
+		
+    }
+
+    public function down()
+    {
+        echo "m150828_075352_create_category_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
